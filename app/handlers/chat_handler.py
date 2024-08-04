d6eb2d2e5c4f47ada87f5fd2cc68c695b7646feb
tarxzf <discordya089@gmail.com>
2024-08-04 21:00:08 +0300

    Added tags support

--- app/handlers/chat_handler.py
+++ app/handlers/chat_handler.py
@@ -1,11 +1,12 @@
-from aiogram import Bot, Router
+from aiogram import Bot, Router, F
 from aiogram.exceptions import TelegramBadRequest
-from aiogram.types import Message
+from aiogram.types import Message, CallbackQuery, InlineKeyboardMarkup
 from asyncio import create_task, gather, sleep
 from typing import List, Optional, Tuple
 
-from data.config import AUTODELETE_MESSAGE_INFO, AUTODELETE_MESSAGE_INFO_DELAY, SHOW_MEDIA_FILE_ID, REPLIES_ENABLED
+from data.config import AUTODELETE_MESSAGE_INFO, AUTODELETE_MESSAGE_INFO_DELAY, SHOW_MEDIA_FILE_ID, REPLIES_ENABLED, TAG_COMMAND_ENABLED
 from data.messages import Messages
+from keyboards.chat_keyboard import ChatKeyboard
 from loader import connection
 from utils.echo import send_message, MessageResponse
 from utils.message import HandleMessageResponse
@@ -15,6 +16,8 @@ router = Router()
 
 @router.message()
 async def get_message_command_handler(message: Message, bot: Bot):
+    user_id = message.from_user.id
+
     # Если отображение файл айди медиа включено:
     if SHOW_MEDIA_FILE_ID:
         media = message.photo or message.animation
@@ -32,22 +35,33 @@ async def get_message_command_handler(message: Message, bot: Bot):
 
             replies = await _fetch_replies(reply_to_message_id)
     
+    # Получаем всех юзеров из базы данных
     cursor = await connection.execute('SELECT id FROM users WHERE blocked = false;')
     rows: list[list[int]] = await cursor.fetchall()
 
     users: list[int] = [i[0] for i in rows]
 
+    # Создаём клавиатуру под сообщением (с тегом)
+    markup = await _get_markup(message, user_id)
+
+    # Отправляем сообщения пользователям
     tasks = []
     for receiver_id in users:
-        tasks.append(create_task(send_message(message, bot, receiver_id, reply_to=replies.get(receiver_id))))
+        task = create_task(
+            coro=send_message(
+                message,
+                bot,
+                receiver_id,
+                reply_markup=markup,
+                reply_to=replies.get(receiver_id)
+            )
+        )
+        tasks.append(task)
     
     result: MessageResponse = await gather(*tasks)
 
-    blocked_users = []  # Список заблокированных пользователей
-    for msg_response in result:
-        if msg_response[1] == False:
-            # Пополняем список, если message_id = False
-            blocked_users.append(tuple(msg_response[0]))
+    # Список заблокированных пользователей
+    blocked_users = [(i[0],) for i in result if i[1] == False]
     
     if blocked_users:
         await connection.executemany(
@@ -73,10 +87,41 @@ async def _delete_message(message: Message):
             # Если сообщеине не было найдено, или уже было удалено пользователем
             ...
 
+
+@router.callback_query(F.data.split()[0] == ChatKeyboard.button_callback_2)
+async def get_user_name_no_url_callback_query_handler(callback: CallbackQuery):
+    await callback.answer(Messages.user_name_no_url_callback_message, True)
+
+
 async def _handle_messages(messages: MessageResponse):
     # Функция для сохранения сообщения в таблицу messages
     handle_message_response = HandleMessageResponse(messages)
     await handle_message_response.save()
+
+
+async def _get_markup(message: Message, user_id: int) -> InlineKeyboardMarkup:
+    markup = None
+    if TAG_COMMAND_ENABLED:
+        async with await connection.execute(
+            'SELECT tag_enabled, url_enabled FROM users WHERE id = ?;',
+            (user_id,)
+        ) as cursor:
+            row: Tuple[bool, bool] = await cursor.fetchone()
+        
+        user_tag_enabled = row[0]
+        user_url_enabled = row[1]
+
+        if user_tag_enabled:
+            user_first_name = message.from_user.first_name
+
+            url = None
+            if user_url_enabled:
+                user_name = message.from_user.username
+                if user_name is not None:
+                    url = f't.me/{user_name}'
+
+            markup = await ChatKeyboard.markup(user_first_name, url=url)
+    return markup
 
 
 async def _fetch_replies(message_id: int):
--- app/utils/echo.py
+++ app/utils/echo.py
@@ -1,6 +1,6 @@
 from aiogram import Bot
 from aiogram.exceptions import TelegramForbiddenError, TelegramBadRequest
-from aiogram.types import Message
+from aiogram.types import Message, InlineKeyboardMarkup
 from typing import NewType, Union, Tuple, Optional
 
 from data.messages import Messages
@@ -17,26 +17,41 @@ MessageId = NewType('MessageId', Union[int | bool | None])
 MessageResponse = NewType('MessageResponse', Tuple[ReceiverId, MessageId])
 
 
-async def send_message(message: Message, bot: Bot, receiver_id: int, *, reply_to: Optional[int] = None) -> MessageResponse:
+async def send_message(
+        message: Message,
+        bot: Bot,
+        receiver_id: int,
+        *,
+        reply_to: Optional[int] = None,
+        reply_markup: Optional[InlineKeyboardMarkup]
+) -> MessageResponse:
     try:
         # Отправляем сообщение пользователю
-        received_message = await message.copy_to(receiver_id, reply_to_message_id=reply_to)
+        received_message = await message.copy_to(
+            chat_id=receiver_id,
+            reply_markup=reply_markup,
+            reply_to_message_id=reply_to
+        )
 
     except TelegramForbiddenError:
         # Пользователь заблокировал бота
         return (receiver_id, False)
     
     except TelegramBadRequest as _ex:
-        if 'Bad Request: message to be replied not found' in _ex:
+        if 'Bad Request: message to be replied not found' in str(_ex):
             # Сообщение, на которое нужно ответить не было найдено
             # Пробуем просто отправить сообщение (без реплая)
             try:
-                received_message = await message.copy_to(receiver_id)
+                received_message = await message.copy_to(receiver_id, reply_markup=reply_markup)
             except Exception as ex_:
                 # Произошла другая ошибка
                 print(f'An error occured while sending the message. Description: {_ex}')
                 return (receiver_id, None)
             return (receiver_id, received_message.message_id)
+        
+        elif 'Bad Request: chat not found' in str(_ex):
+            # Пользователь из базы данных никогда не взаимодействовал с ботом
+            return (receiver_id, False)
             
         # Произошла другая ошибка связанная с TelegramBadRequest
         try:
